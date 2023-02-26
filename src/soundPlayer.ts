dd5cb00873e3413312bb8525ec51cf1b9103b4ba
Nico Enking <nico.enking@gmail.com>
2023-02-26 10:17:48 +0100

    Use VLC as sound player

--- src/soundPlayer.ts
+++ src/soundPlayer.ts
@@ -1,15 +1,21 @@
-import { AlarmType } from './AlarmType';
+import { AlarmType } from "./AlarmType";
 import path from "path";
 import SoundPlay from "sound-play";
 import { existsSync } from "fs";
 import { mkdir, writeFile } from "fs/promises";
-import { Bucket } from '@google-cloud/storage';
+import { Bucket } from "@google-cloud/storage";
+import which from "which";
+import { exec } from "child_process";
 
 export class SoundPlayer {
   static readonly cachePath = path.join(__dirname, "..", "cache");
   static readonly assetsPath = path.join(__dirname, "assets");
 
-  static async playAlarm(alarmType: AlarmType, bucketPath: string, bucket: Bucket) {
+  static async playAlarm(
+    alarmType: AlarmType,
+    bucketPath: string,
+    bucket: Bucket
+  ) {
     await this.playGong(alarmType);
     const localPath = await this.downloadTtsFile(bucketPath, bucket);
     await this.playTtsFile(localPath);
@@ -19,7 +25,9 @@ export class SoundPlayer {
     let gongPath = this.getGongPath(alarmType);
 
     if (gongPath === null) {
-      console.debug(`Not playing gong because gongPath for gong ${alarmType} is null`);
+      console.debug(
+        `Not playing gong because gongPath for gong ${alarmType} is null`
+      );
       return;
     }
 
@@ -29,13 +37,16 @@ export class SoundPlayer {
 
     console.debug(`Playing Gong ${alarmType} from ${gongPath}`);
 
-    await SoundPlay.play(gongPath);
+    await this.play(gongPath);
   }
 
   private static getGongPath(alarmType: AlarmType) {
-    switch(alarmType) {
+    switch (alarmType) {
       case AlarmType.EINZELFAHRZEUGALARM:
-        return path.join(SoundPlayer.assetsPath, "einzelfahrzeug-alarm_mit_gong.wav");
+        return path.join(
+          SoundPlayer.assetsPath,
+          "einzelfahrzeug-alarm_mit_gong.wav"
+        );
       case AlarmType.VORALARM:
         return path.join(SoundPlayer.assetsPath, "vor-alarm_mit_gong.wav");
       case AlarmType.ZUGALARM:
@@ -50,12 +61,14 @@ export class SoundPlayer {
   private static async downloadTtsFile(bucketPath: string, bucket: Bucket) {
     const file = bucket.file(bucketPath);
     const localPath = path.join(SoundPlayer.cachePath, file.name);
-    
+
     if (!existsSync(localPath)) {
-      console.debug(`File ${file.name} does not exist. Downloading from Storage.`);
+      console.debug(
+        `File ${file.name} does not exist. Downloading from Storage.`
+      );
       await mkdir(path.dirname(localPath), { recursive: true });
       const [mp3] = await file.download();
-      await writeFile(localPath, mp3, );
+      await writeFile(localPath, mp3);
     } else {
       console.debug(`File ${file.name} already existed. Returning cache path.`);
     }
@@ -65,11 +78,38 @@ export class SoundPlayer {
 
   private static async playTtsFile(localPath: string) {
     console.debug(`Playing tts file ${localPath}`);
-    await SoundPlay.play(localPath);
+    await this.play(localPath);
   }
 
   static async playStartupSound() {
     const startupPath = path.join(SoundPlayer.assetsPath, "winxp.mp3");
-    await SoundPlay.play(startupPath);
+    await this.play(startupPath);
+  }
+
+  private static async play(path: string) {
+    let vlcExec;
+    if (process.platform === "win32") {
+      vlcExec = "vlc.exe";
+    } else {
+      vlcExec = "vlc";
+    }
+
+    const vlcPath = await which(vlcExec, { nothrow: true });
+    if (vlcPath === null) {
+      throw new Error(
+        "Could not find vlc on device. Please install vlc and if under windows, add it's installation folder to PATH environment variable."
+      );
+    }
+    return new Promise<void>((res, rej) => {
+      const vlcProcess = exec(`${vlcPath} -Idummy ${path} vlc://quit`);
+
+      vlcProcess.on("exit", (code) => {
+        if (code === 0) {
+          res();
+        } else {
+          rej(vlcProcess.stderr);
+        }
+      });
+    });
   }
 }
